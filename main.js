a605352db6b287a49014abdf8cfaed24660ff3f7
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-04-26 06:51:58 +0600

    second commit

--- main.js
+++ main.js
@@ -52,25 +52,46 @@ const data =  JSON.parse((localStorage.getItem('users')))
     function addToStore() {
         if(data.find(item => item.email === email.value)){
             email.value = '';
-            return alert("this email already be taken")
+            email.addEventListener('click', () => {
+                document.getElementById('emailspan').innerHTML = `<span id="emailspan"></span>`
+              });
+            return document.getElementById('emailspan').innerHTML = `<span class="passwordred">this email is already in use</span>`
         }
         if(data.find(item => item.username === username.value)){
             username.value = '';
-            return alert("this username already be taken")
+            username.addEventListener('click', () => {
+                document.getElementById('namespan').innerHTML = `<span id="namespan"></span>`
+              });
+            return document.getElementById('namespan').innerHTML = `<span class="passwordred">this name is already in use</span>`
         }
         if (password1.value === password2.value){
              password = password1
-        }else if(password1.value !== password2.value){
-            return alert('diferent password')
+        }
+        if(password1.value !== password2.value){
+            return document.getElementById('passwordspan').innerHTML = `<span class="passwordred">Password don't match</span>`
         }
         if(password1.value.length === 0 || password2.value.length === 0){
-            return alert('type password')
+            password1.addEventListener('click', () => {
+                document.getElementById('passwordspan').innerHTML = `<span id="passwordspan"></span>`
+              });
+            return document.getElementById('passwordspan').innerHTML = `<span class="passwordred">Enter password to continue</span>`
         }else if(email.value == ''){
-            return alert('type email')
+            email.addEventListener('click', () => {
+                document.getElementById('emailspan').innerHTML = `<span id="emailspan"></span>`
+              });
+            return document.getElementById('emailspan').innerHTML = `<span class="passwordred">Enter Email to continue</span>`
         }else if(username.value == '' ){
-            return alert('type your name')
-        }else if(password1.value.length <= 8) {
-            return alert('short password')
+            username.addEventListener('click', () => {
+                document.getElementById('namespan').innerHTML = `<span id="namespan"></span>`
+              });
+            return document.getElementById('namespan').innerHTML = `<span class="passwordred">Enter your name to continue</span>`
+        }else if(password1.value.length <= 7) {
+            password1.addEventListener('click', () => {
+                password1.value = ""
+                password2.value = ""
+                document.getElementById('passwordspan').innerHTML = `<span id="passwordspan"></span>`
+              });
+            return document.getElementById('passwordspan').innerHTML = `<span class="passwordred">Please enter at least 8 characters</span>`
         }else if(username.value > 0  || email.value > 0  || password1.value.length >= 8) {
             data.push({
                 username: username.value,
